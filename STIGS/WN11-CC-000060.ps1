987399736d15f587c128ba43b9c92e15c0aa187e
Mae Menges <mae.menges@gmail.com>
2026-05-18 13:25:35 -0700

    Enhance WN11-CC-000060.ps1 with documentation
    Added metadata and usage instructions to the script.

--- STIGS/WN11-CC-000060.ps1
+++ STIGS/WN11-CC-000060.ps1
@@ -1,1 +1,26 @@
+<#
+.SYNOPSIS
+    This script configures Windows policy to prevent the system from maintaining connections to both domain and non-domain networks at the same time.
 
+.NOTES
+    Author          : Mae Menges
+    LinkedIn        : linkedin.com/in/mae-menges
+    GitHub          : github.com/maemenges
+    Date Created    : 2026-05-18
+    Last Modified   : 2026-05-18
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-00-000210
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-00-000210/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-00-000210
